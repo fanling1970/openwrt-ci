61824816be802b14986cb74dc45934368a3f8ced
fanling1970 <zghqidai2008@sina.com>
2026-05-12 21:28:52 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -2,7 +2,7 @@
 ./scripts/feeds update -a
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.192.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # TTYD 免登录
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
